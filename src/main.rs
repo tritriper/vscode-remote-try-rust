1be3a9d042864d7aee27cee56dc3db85cf2dd88f
Tristan Perrault <tritriper35@gmail.com>
2022-05-29 09:59:18 +0000

    [HW-EVOL] Rework main.rs to run the "Hello fellow Rustaceans!"

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,26 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
+ 
+ extern crate ferris_says;
+
+ use ferris_says::say; 
+ use std::io::{stdout, BufWriter};
 
 fn main() {
-    let name = "VS Code Remote - Containers";
-    println!("Hello, {}!", name);
+
+    return test_ferris();
+    
+}
+
+/**
+ * Display the hello work from ferris_says crate
+ */
+fn test_ferris() {
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans!");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
 }
